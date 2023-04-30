e846504d36a5b9c800c5d873609367fc21c639f0
nadabazzal <127792861+nadabazzal@users.noreply.github.com>
2023-04-30 14:06:49 +0300

    Add files via upload

---
+++ NadaBazzal/java.js
@@ -0,0 +1,13 @@
+window.addEventListener('load', function(){document.getElementById('preloader').style.display='none'})
+
+function normal(x) {
+    x.style.height ='100px';
+    x.style.width='150px';
+   
+  }
+  function big(x) {
+    x.style.height ='400px';
+    x.style.width='750px';
+   
+  }
+ 
